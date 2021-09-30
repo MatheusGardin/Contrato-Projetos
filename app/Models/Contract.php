43ac1c78ca2c88971d462acc939cf67c9ccc0a5c
MatheusGardin <jackmatheus123@gmail.com>
2021-09-29 22:50:37 -0300

    Models and migrations

---
+++ app/Models/Contract.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Contract extends Model {
+    use HasFactory, softDeletes;
+}
