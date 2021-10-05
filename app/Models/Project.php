f859036daea846b8e53be0e15deed87922be233a
MatheusGardin <jackmatheus123@gmail.com>
2021-10-05 09:07:13 -0300

    FormRequest created

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -7,5 +7,12 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Project extends Model {
+
     use HasFactory, softDeletes;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'planned_hours'
+    ];
 }
